00e0544dcff71ad52c6f3eb1fa724ac8ef789caf
yurik4125 <yurik4125@gmail.com>
2019-04-23 23:49:33 -0400

    Before create Master page

--- CarShop/Controllers/HomeController.cs
+++ CarShop/Controllers/HomeController.cs
@@ -3,27 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Antlr.Runtime;
+using CarShop.Models;
 
 namespace CarShop.Controllers
 {
     public class HomeController : Controller
     {
+        // GET: Home
+        private  Models.ShopDBEntities db = new ShopDBEntities();
         public ActionResult Index()
         {
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
+            var  Items = db.Cars;
             return View();
         }
     }
